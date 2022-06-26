4fa9a484d3fda1f40a1ea4dc33ec2bf0e492765c
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-06-26 16:36:29 -0500

    Add files via upload

---
+++ Mapping_Multiple_GeoJSON_Points/static/jss/logic.js
@@ -0,0 +1,37 @@
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'streets' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/JaimeStarling/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+        pointToLayer(feature, latlng)
+        console.log(feature);
+        return L.marker(latlng)
+            .bindPopup(`<h2>Airport code: ${feature.properties.faa}</h2> <h3>Airport name: ${feature.properties.name}</h3>`);        
+});
+
+
+// Grabbing our GeoJSON data.
+//L.geoJson(sanFranAirport, {
+    // We turn each feature into a Marker on the map.
+  //  pointToLayer(feature, latlng) {
+    //  console.log(feature);
+      //return L.marker(latlng)
+        //.bindPopup(`<h2>Airport code: ${feature.properties.faa}</h2> <h3>Airport name: ${feature.properties.name}</h3>`);
+   // },
+  
+ // }).addTo(map);
