f40bcd9db751356a7123775df9cea5d13fb764f8
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-06-26 14:23:21 -0500

    Add files via upload

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,32 +5,40 @@ console.log("working");
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+const sanFranAirport = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      properties: {
+        id: '3469',
+        name: 'San Francisco International Airport',
+        city: 'San Francisco',
+        country: 'United States',
+        faa: 'SFO',
+        icao: 'KSFO',
+        alt: '13',
+        'tz-offset': '-8',
+        dst: 'A',
+        tz: 'America/Los_Angeles',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.375, 37.61899948120117],
+      },
+    },
+    ],
+  };
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup();
-     }
-}).addTo(map);
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a Marker on the map.
+    pointToLayer(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+        .bindPopup(`<h2>Airport code: ${feature.properties.faa}</h2> <h3>Airport name: ${feature.properties.name}</h3>`);
+    },
+  
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
